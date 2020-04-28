256b731ff73e9d6418c8793c9b05b9b316d4b461
sspiff <sspiff@users.noreply.github.com>
2020-04-28 14:14:51 -0400

    getTagsFor/lambda: call listTags using region of master arn
    The region of the master arn can differ from that of the original arn,
    e.g. in the case of lambda@edge.

--- getTagsFor/lambda.js
+++ getTagsFor/lambda.js
@@ -3,17 +3,18 @@ import awsClient from '../client'
 import parseArn from '../parseArn'
 
 
-export default arn => {
-  const L = awsClient('lambda', {region: parseArn(arn).region})
-  return (
-    // get the master arn, if any
-    L.getFunctionConfiguration({FunctionName: arn}).promise()
-    .then(data => data.MasterArn || arn)
-    // remove any version number
-    .then(arn => arn.split(':').slice(0, 7).join(':'))
-    // finally get and return the tags
-    .then(arn => L.listTags({Resource: arn}).promise())
-    .then(data => data.Tags || {})
-  )
-}
+export default arn =>
+  awsClient('lambda', {region: parseArn(arn).region})
+  // get the master arn, if any
+  .getFunctionConfiguration({FunctionName: arn}).promise()
+  .then(data => data.MasterArn || arn)
+  // remove any version number
+  .then(arn => arn.split(':').slice(0, 7).join(':'))
+  // finally get and return the tags
+  .then(arn =>
+      // region of master arn may be different from that of original arn
+      awsClient('lambda', {region: parseArn(arn).region})
+      .listTags({Resource: arn}).promise()
+    )
+  .then(data => data.Tags || {})
 
